0fed8282071f6a7ab3c6da1507a56d59d52d3d2b
Akshat <tiwariakshat343@gmail.com>
2025-08-18 13:33:03 +0530

    back-end integrations to generate webhooks link

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js';
 import * as Sentry from "@sentry/node";
+import { clerkWebhooks } from './controllers/webhooks.js';
 //Initializing Express
 const app = express();
 
@@ -23,6 +24,8 @@ app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
 
+app.post('/webhooks',clerkWebhooks)
+
 
 const PORT = process.env.PORT || 5000
 
@@ -31,3 +34,4 @@ Sentry.setupExpressErrorHandler(app);
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
 })
+
